63b5a85c37bc1d4fcd15b45d4feb7c541238d478
nik <n.cowalyow2017@yandex.ru>
2025-06-09 22:25:33 +0300

    add loaded

---
+++ scripts/loaded.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    console.log('Все теги загрузились');
+});
+
+window.addEventListener('load', ()=>{
+    console.log('Вся страница загрузилась');
+});
