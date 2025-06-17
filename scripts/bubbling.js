f19e5ab3baa0969cd093cea6f878a8a3016c1844
nik <n.cowalyow2017@yandex.ru>
2025-06-17 10:29:28 +0300

    add checkbox off bubbling

--- scripts/bubbling.js
+++ scripts/bubbling.js
@@ -1,20 +1,30 @@
 const [el1, el2, el3] = [document.getElementById('elem1'), document.getElementById('elem2'), document.getElementById('elem3')];
-
+const btnOffBubbling = document.querySelector(`input[id='check']`);
 
 const listColor= ['b-green', 'b-white', 'b-red', 'b-cyan'];
 const getRandColor=()=> listColor[Math.floor(Math.random()*(listColor.length))];
 
-el1.addEventListener('click',function(){
+
+el1.addEventListener('click',function(event){
+    if (btnOffBubbling.checked){
+        event.stopPropagation();
+    }
     this.classList='';
     this.classList.add(getRandColor());
 });
 
-el2.addEventListener('click',function(){
+el2.addEventListener('click',function(event){
+    if (btnOffBubbling.checked){
+        event.stopPropagation();
+    }
     this.classList='';
     this.classList.add(getRandColor());
 })
 
-el3.addEventListener('click',function(){
+el3.addEventListener('click',function(event){
+    if (btnOffBubbling.checked){
+        event.stopPropagation();
+    }
     this.classList='';
     this.classList.add(getRandColor());
 })
