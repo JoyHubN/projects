297c1c3d5b6b93038dde21248e1c57b215075b13
nik <n.cowalyow2017@yandex.ru>
2025-06-17 11:51:52 +0300

    add counter mouseover

--- scripts/bubbling.js
+++ scripts/bubbling.js
@@ -1,5 +1,6 @@
 const [el1, el2, el3] = [document.getElementById('elem1'), document.getElementById('elem2'), document.getElementById('elem3')];
 const btnOffBubbling = document.querySelector(`input[id='check']`);
+const count = document.querySelector('#count');
 
 const listColor= ['b-green', 'b-white', 'b-red', 'b-cyan'];
 const getRandColor=()=> listColor[Math.floor(Math.random()*(listColor.length))];
@@ -19,7 +20,7 @@ el2.addEventListener('click',function(event){
     }
     this.classList='';
     this.classList.add(getRandColor());
-})
+});
 
 el3.addEventListener('click',function(event){
     if (btnOffBubbling.checked){
@@ -27,4 +28,8 @@ el3.addEventListener('click',function(event){
     }
     this.classList='';
     this.classList.add(getRandColor());
+});
+
+count.addEventListener('mouseover', function(){
+    this.textContent = Number(this.textContent)+1
 })
